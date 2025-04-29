2130dcbd0bde35638e6589d58068eeff2bef8cc5
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-04-29 16:07:34 -0400

    implemented pictures service

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,8 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return jsonify(data), 200
+
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +45,13 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    picture = next((pic for pic in data if pic["id"] == id), None)
+    
+    if picture is None:
+        abort(404)
+    
+    return jsonify(picture), 200
+
 
 
 ######################################################################
@@ -52,20 +59,55 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    new_picture = request.get_json()  # Extract picture data from the request
+    
+    # Check if the picture with the same id already exists
+    if any(pic["id"] == new_picture["id"] for pic in data):
+        return jsonify({"Message": f"picture with id {new_picture['id']} already present"}), 302
+    
+    # If no duplicates, add the new picture to the data list
+    data.append(new_picture)
+    
+    # Return the newly added picture with a 201 Created status code
+    return jsonify(new_picture), 201
+
 
 ######################################################################
 # UPDATE A PICTURE
 ######################################################################
-
-
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    # Extract the new picture data from the request body
+    updated_picture = request.get_json()
+
+    # Find the picture by id in the data list
+    picture = next((pic for pic in data if pic["id"] == id), None)
+    
+    if picture is None:
+        # If the picture doesn't exist, return a 404 error
+        return jsonify({"message": "picture not found"}), 404
+
+    # If picture exists, update the fields
+    picture.update(updated_picture)
+
+    # Return the updated picture with a 200 status code
+    return jsonify(picture), 200
+
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    # Find the picture by id
+    picture = next((pic for pic in data if pic["id"] == id), None)
+
+    if picture is None:
+        # If the picture is not found, return a 404 error
+        return jsonify({"message": "picture not found"}), 404
+
+    # If the picture exists, delete it
+    data.remove(picture)
+
+    # Return a 204 No Content status with an empty body
+    return '', 204
